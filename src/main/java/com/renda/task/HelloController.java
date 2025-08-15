2a9565741ca987fc13bea8bfb53dff755d3da05c
Renda Zhang <rendazhang@qq.com>
2025-08-15 19:07:45 +0800

    Add basic pre-commit related stuff and fixing java code

--- src/main/java/com/renda/task/HelloController.java
+++ src/main/java/com/renda/task/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/api/hello")
-    public String hello(@RequestParam(defaultValue = "World") String name) {
+    public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") String name) {
         return "hello " + name;
     }
 
